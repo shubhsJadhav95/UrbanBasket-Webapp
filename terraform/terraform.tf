27dbfee73ec99948724d94eb4c8153978a1e1ede
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2026-06-02 22:23:45 +0530

    terraform env updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "terraform-s3-urbanbucket"
     key          = "backend-locking"
-    region       = "eu-west-1"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region where resources will be provisioned"
-  default     = "us-east-2"
+  default     = "us-east-1"
 }
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-05cf1e9f73fbad2e2"
 }
 
 variable "instance_type" {
